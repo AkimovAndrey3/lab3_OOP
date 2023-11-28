943b56f9fccf9655947d2080068ae59ed18092bc
Andrey <akimova.andr@yandex.ru>
2023-11-28 21:22:48 +0300

    ЕГОР ХОРОШИЙ

--- lab3 av/Program.cs
+++ lab3 av/Program.cs
@@ -17,7 +17,7 @@ namespace lab3_av
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // ЛОГАНОВ ПИДОР
+            //ЕГОР ЛУЧШИЙ
         }
     }
 }
