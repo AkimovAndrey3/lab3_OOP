6cd5e0aeea3dff28c6627195e190581a6392c84d
AkimovAndrey3 <akimova.andr@yandex.ru>
2023-11-29 00:24:01 +0300

    123

--- lab3 av/Program.cs
+++ lab3 av/Program.cs
@@ -17,6 +17,7 @@ namespace lab3_av
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //123
         }
     }
 }
